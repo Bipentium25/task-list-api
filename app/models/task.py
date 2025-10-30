83f98dfd6fbe6b6f642f6b8206457474c12c6619
DayDayUp <bipentium25@gmail.com>
2025-10-30 15:25:49 -0700

    stuck on from dict

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,25 @@
+from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]]
+
+
+    def to_dict(self):
+        is_complete = True if self.completed_at else False
+        return {
+                "id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete":is_complete
+        }
+    
+    def from_dict(self, task_dict):
+        self.title = task_dict.get("title")
+        self.description = task_dict.get("description")
+        self.completed_at = None if task_dict is False else task_dict.get("is_complete")
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,44 @@
-from flask import Blueprint
+
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from ..db import db
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body["is_complete"]
+
+    new_task = Task(title=title, description=description,completed_at=completed_at)
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+        "id": new_task.id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "is_complete":new_task.completed_at
+    }
+    return response, 201
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(
+            {
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete":task.completed_at
+            }
+        )
+    return tasks_response
